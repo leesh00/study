11ee93478a47b40c6eac4af3753b29e1fa4195a3
sohiLee <lsohi2468@gmail.com>
2025-11-02 18:17:31 +0900

    react-공통 컴포넌트

--- react/emotion_diary/src/App.jsx
+++ react/emotion_diary/src/App.jsx
@@ -3,33 +3,19 @@ import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
+import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/get-emotion-image'
+import Button from './components/Button'
+import Header from './components/Header'
 
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav("/new");
-  };
   return (
-    <>
-    <div>
-      <img src={getEmotionImage(1)} />
-      <img src={getEmotionImage(2)} />
-      <img src={getEmotionImage(3)} />
-      <img src={getEmotionImage(4)} />
-      <img src={getEmotionImage(5)} />
-    </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>new</Link>
-        <Link to={"/diary"}>diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 으로 이동</button>
+    <> 
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/new' element={<New />}></Route>
         <Route path='/diary/:id' element={<Diary />}></Route>
+        <Route path='/edit/:id' element={<Edit />}></Route>
         <Route path='*' element={<Notfound />}></Route>
       </Routes>
     </>
